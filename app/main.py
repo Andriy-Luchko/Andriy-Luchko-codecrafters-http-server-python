fea78ac35b69bdaf861c972fa447a71e222463cf
Andriy-Luchko <andriy.luchko@gmail.com>
2024-04-22 13:36:32 -0400

    get file from /files/<filename> test 1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import argparse 
+import os 
 
 successResponse = "HTTP/1.1 200 OK\r\n\r\n".encode()
 failureResponse = "HTTP/1.1 404 NOT FOUND\r\n\r\n".encode()
@@ -13,15 +15,19 @@ def parse_path(data):
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory", help="serving files dir")
+    args = parser.parse_args()
+
     while True:
         connection, address = server_socket.accept()
-        responseThread = threading.Thread(target=handleReq, args=(connection,))
+        responseThread = threading.Thread(target=handleReq, args=(connection, args.directory))
         responseThread.start()
 
 
 
 
-def handleReq(connection):
+def handleReq(connection, directory):
     data = connection.recv(1024)
     path = parse_path(data)
     if b"/" == path:
@@ -46,7 +52,17 @@ def handleReq(connection):
             contentLength = str(len(content)).encode()
             res = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + contentLength + b"\r\n\n" + content + b"\n\r\n"
             connection.send(res)
-            
+        elif b"files" == pathArray[1]:
+            fileName = pathArray[2]
+            path = os.path.join(directory, fileName.decode("ascii"))
+            if os.path.isfile(path):
+                with open(path, "rb") as file:
+                    fileBody = file.read().decode("ascii")
+                    print(fileBody)
+                    content = fileBody
+                    contentLength = str(len(content)).encode("ascii")
+                    res = b"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + contentLength + b"\r\n\n" + content + b"\n\r\n"
+                    connection.send(res)
         else:
             connection.send(failureResponse)
 
