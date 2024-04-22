3459b5ee66f81ad11753e858299849ec767e0dfe
Andriy-Luchko <andriy.luchko@gmail.com>
2024-04-22 00:18:46 -0400

    /user-agent returning user agent to client

--- app/main.py
+++ app/main.py
@@ -12,26 +12,33 @@ def parse_path(data):
     return b""
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connection, address = server_socket.accept() # wait for client
+    connection, address = server_socket.accept()
     data = connection.recv(1024)
     path = parse_path(data)
-
     if b"/" == path:
         connection.send(successResponse)
     else:
         pathArray = path.split(b"/")
 
         if b"echo" == pathArray[1]:
+            # echo the rest of the path to the client
             content = b"/".join(pathArray[2:])
             contentLength = str(len(content)).encode()
             res = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + contentLength + b"\r\n\n" + content + b"\n\r\n"
             connection.send(res)
+        elif b"user-agent" == pathArray[1]:
+            # figure out user agent
+            startIdx = data.find(b"User-Agent: ") + len(b"User-Agent: ")
+            endIdx = startIdx + data[startIdx:].find(b"\r\n") 
+            userAgent = data[startIdx:endIdx]
+
+            # respond back to client
+            content = userAgent
+            contentLength = str(len(content)).encode()
+            res = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + contentLength + b"\r\n\n" + content + b"\n\r\n"
+            connection.send(res)
+            
         else:
             connection.send(failureResponse)
 
