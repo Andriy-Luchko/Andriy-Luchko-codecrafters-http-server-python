fa8147d71b5f0c4d2f3916102ae909d7b8ee0c16
Andriy-Luchko <andriy.luchko@gmail.com>
2024-04-21 23:48:56 -0400

    respond 200 and rest of path to  /echo/

--- app/main.py
+++ app/main.py
@@ -4,6 +4,13 @@ import socket
 successResponse = "HTTP/1.1 200 OK\r\n\r\n".encode()
 failureResponse = "HTTP/1.1 404 NOT FOUND\r\n\r\n".encode()
 
+def parse_path(data):
+    lines = data.split(b"\r\n")
+    if len(lines) > 0:
+        path = lines[0].split(b" ")[1]
+        return path
+    return b""
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -13,10 +20,22 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
     data = connection.recv(1024)
-    if b" / " in data:
+    path = parse_path(data)
+
+    if b"/" == path:
         connection.send(successResponse)
     else:
-        connection.send(failureResponse)
+        pathArray = path.split(b"/")
+
+        if b"echo" == pathArray[1]:
+            content = b"/".join(pathArray[2:])
+            contentLength = str(len(content)).encode()
+            res = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + contentLength + b"\r\n\n" + content + b"\n\r\n"
+            connection.send(res)
+        else:
+            connection.send(failureResponse)
+
+
         
 
 
